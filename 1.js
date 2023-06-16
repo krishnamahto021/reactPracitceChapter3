8f5ee7d6f744de271483ab2632c6f9004bc153f1
KRISHNA MAHATO <mkrishna98212@gmail.com>
2023-06-16 16:43:09 +0530

    create a mini CMT

--- 1.js
+++ 1.js
@@ -1,49 +1,86 @@
-const quotes = [];
+let customers = [];
+const capacity = 25;
+let seatsLeft = 25;
+const currentTime = new Date();
 
-let quoteRef = React.createRef();
+let guestCountRef = React.createRef();
 let nameRef = React.createRef();
+let phoneRef = React.createRef();
 
-
-function handleSubmit(event){
+// Create form submit handler here
+function handleSubmit(event) {
     event.preventDefault();
-    let newQuote = ({
+    seatsLeft = seatsLeft - guestCountRef.current.value;
+    let newCustomer = ({
         name:nameRef.current.value,
-        quote:quoteRef.current.value
+        phone:phoneRef.current.value,
+        guestCount:guestCountRef.current.value
     });
-    quotes.push(newQuote);
-    rootElement.render(<App/>);
+    customers.push(newCustomer);
+    console.log(customers);
+    rootElement.render(<App />);
+
 }
 
-const clearInputs = () => {
-  nameRef.current.value = "";
-  quoteRef.current.value = "";
-};
+const Table = () => (
+    <table border="1px" style={{ margin: "auto" }}>
+    <thead>
+        <tr>
+            <th>count</th>
+            <th>Name</th>
+            <th>Phone</th>
+            <th>checkIn</th>
+            <th>Checkout</th>
+            <th>Status</th>
+            <th>Remove</th>
+        </tr>
+    </thead>
+
+    <tbody>
+
+            {customers.map((customer,index)=>(
+                <tr key={index}>
+                <td>{customer.guestCount}</td>
+                <td>{customer.name}</td>
+                <td>{customer.phone}</td>
+                <td>{currentTime.toLocaleTimeString([], { hour12: true, hour: '2-digit', minute: '2-digit', second: '2-digit' })}</td>
+                <td>-</td>
+                <td>Remove</td>
+                <td>Delete Customer</td>
+
+                </tr>
+            ))}
+
+    </tbody>
+
+
+
+
+    </table>
+)
 
 const Form = () => (
     <>
         <form onSubmit={handleSubmit}>
-            <input type="text" placeholder='Quote' ref={quoteRef} />
-            <input type="text" placeholder='Name' ref={nameRef} />
-            <button type='submit'>Submit</button>
+            <input type="number" placeholder='Guest Count' ref={guestCountRef} /><br />
+            <input type="text" placeholder='Primary Guest Name' ref={nameRef} /><br />
+            <input type="number" placeholder='Phone Number' ref={phoneRef} /><br />
+            <button type='submit'>Create Entry</button>
         </form>
     </>
 )
 
-// create handleQuote function here
-
 const App = () => (
-  <div className="App">
-    <h2>Quotify</h2>
-    <Form/>
-    <div className="quotes">
-      {quotes.map((q, i) => (
-        <div key={i}>
-          <i>"{q.quote}"</i>
-          <b>~ {q.name}</b>
+    <>
+        <div className="App" style={{ textAlign: "center" }}>
+            <div>
+                <h2>Total Capacity:{capacity} </h2>
+                <h2>Seats Left:{seatsLeft} </h2>
+            </div>
         </div>
-      ))}
-    </div>
-  </div>
+        <Form />
+        <Table />
+    </>
 );
 
 const rootElement = ReactDOM.createRoot(document.getElementById("root"));

76ea789db18515f36e8a0c199098dbcbe0c05fa3
KRISHNA MAHATO <mkrishna98212@gmail.com>
2023-06-16 16:56:40 +0530

    make student class routine

--- 1.js
+++ 1.js
@@ -1,87 +1,202 @@
-let customers = [];
-const capacity = 25;
-let seatsLeft = 25;
-const currentTime = new Date();
-
-let guestCountRef = React.createRef();
-let nameRef = React.createRef();
-let phoneRef = React.createRef();
-
-// Create form submit handler here
-function handleSubmit(event) {
-    event.preventDefault();
-    seatsLeft = seatsLeft - guestCountRef.current.value;
-    let newCustomer = ({
-        name:nameRef.current.value,
-        phone:phoneRef.current.value,
-        guestCount:guestCountRef.current.value
-    });
-    customers.push(newCustomer);
-    console.log(customers);
-    rootElement.render(<App />);
-
-}
-
-const Table = () => (
-    <table border="1px" style={{ margin: "auto" }}>
-    <thead>
-        <tr>
-            <th>count</th>
-            <th>Name</th>
-            <th>Phone</th>
-            <th>checkIn</th>
-            <th>Checkout</th>
-            <th>Status</th>
-            <th>Remove</th>
-        </tr>
-    </thead>
-
-    <tbody>
-
-            {customers.map((customer,index)=>(
-                <tr key={index}>
-                <td>{customer.guestCount}</td>
-                <td>{customer.name}</td>
-                <td>{customer.phone}</td>
-                <td>{currentTime.toLocaleTimeString([], { hour12: true, hour: '2-digit', minute: '2-digit', second: '2-digit' })}</td>
-                <td>-</td>
-                <td>Remove</td>
-                <td>Delete Customer</td>
-
-                </tr>
-            ))}
-
-    </tbody>
-
-
-
-
-    </table>
-)
-
-const Form = () => (
-    <>
-        <form onSubmit={handleSubmit}>
-            <input type="number" placeholder='Guest Count' ref={guestCountRef} /><br />
-            <input type="text" placeholder='Primary Guest Name' ref={nameRef} /><br />
-            <input type="number" placeholder='Phone Number' ref={phoneRef} /><br />
-            <button type='submit'>Create Entry</button>
-        </form>
-    </>
-)
+const timeTable = {
+    Monday: {
+        9: {
+            subject: "Maths",
+            teacher: "Brijesh Mishra",
+            time: "1 hour"
+        },
+        10: {
+            subject: "Physics",
+            teacher: "Sumit Yadav",
+            time: "1 hour"
+        },
+        11: {
+            subject: "Chemistry",
+            teacher: "Vedanti Joshi",
+            time: "1 hour"
+        },
+        13: {
+            subject: "English Litrature",
+            teacher: "Mayur Pandey",
+            time: "1 hour"
+        },
+        14: {
+            subject: "Hindi Vyakaran",
+            teacher: "Sony Desai",
+            time: "1 hour"
+        },
+        15: {
+            subject: "PT",
+            teacher: "Sonu Tyagi",
+            time: "1 hour"
+        }
+    },
+    Tuesday: {
+        9: {
+            subject: "Physics",
+            teacher: "Sumit Yadav",
+            time: "1 hour"
+        },
+        10: {
+            subject: "Hindi Sahitya",
+            teacher: "Ramesh Jha",
+            time: "1 hour"
+        },
+        11: {
+            subject: "Environtal Studies",
+            teacher: "Rakesh Prakash",
+            time: "1 hour"
+        },
+        13: {
+            subject: "Biology",
+            teacher: "Kuldeep Narayan",
+            time: "1 hour"
+        },
+        14: {
+            subject: "Maths",
+            teacher: "Brijesh Mishra",
+            time: "1 hour"
+        },
+        15: {
+            subject: "Physics Practical",
+            teacher: "Sumit Yadav",
+            time: "1 hour"
+        }
+    },
+    Wednesday: {
+        9: {
+            subject: "Chemistry",
+            teacher: "Vedanti Joshi",
+            time: "1 hour"
+        },
+        10: {
+            subject: "English Grammar",
+            teacher: "Aditi Upasane",
+            time: "1 hour"
+        },
+        11: {
+            subject: "Maths",
+            teacher: "Brijesh Mishra",
+            time: "1 hour"
+        },
+        13: {
+            subject: "English Litrature",
+            teacher: "Mayur Pandey",
+            time: "1 hour"
+        },
+        14: {
+            subject: "Physics",
+            teacher: "Sumit Yadav",
+            time: "1 hour"
+        },
+        15: {
+            subject: "Chemistry Practical",
+            teacher: "Vedanti Joshi",
+            time: "1 hour"
+        }
+    },
+    Thursday: {
+        9: {
+            subject: "Biology",
+            teacher: "Kuldeep Narayan",
+            time: "1 hour"
+        },
+        10: {
+            subject: "Hindi Sahitya",
+            teacher: "Ramesh Jha",
+            time: "1 hour"
+        },
+        11: {
+            subject: "Chemistry",
+            teacher: "Vedanti Joshi",
+            time: "1 hour"
+        },
+        13: {
+            subject: "Maths",
+            teacher: "Brijesh Mishra",
+            time: "1 hour"
+        },
+        14: {
+            subject: "Physics",
+            teacher: "Sumit Yadav",
+            time: "1 hour"
+        },
+        15: {
+            subject: "Biology Practical",
+            teacher: "Kuldeep Narayan",
+            time: "1 hour"
+        }
+    },
+    Friday: {
+        9: {
+            subject: "English Litrature",
+            teacher: "Mayur Pandey",
+            time: "1 hour"
+        },
+        10: {
+            subject: "Hindi Sahitya",
+            teacher: "Ramesh Jha",
+            time: "1 hour"
+        },
+        11: {
+            subject: "English Grammar",
+            teacher: "Aditi Upasane",
+            time: "1 hour"
+        },
+        13: {
+            subject: "Biology",
+            teacher: "Kuldeep Narayan",
+            time: "1 hour"
+        },
+        14: {
+            subject: "Hindi Vyakaran",
+            teacher: "Sony Desai",
+            time: "1 hour"
+        },
+        15: {
+            subject: "PT",
+            teacher: "Sonu Tyagi",
+            time: "1 hour"
+        }
+    }
+};
 
 const App = () => (
-    <>
-        <div className="App" style={{ textAlign: "center" }}>
-            <div>
-                <h2>Total Capacity:{capacity} </h2>
-                <h2>Seats Left:{seatsLeft} </h2>
-            </div>
-        </div>
-        <Form />
-        <Table />
-    </>
-);
+    <table border="1">
+      <caption>School time-table of a class 12th Student.</caption>
+      <thead>
+        <tr>
+          <th>Day</th>
+          <th>9am-10am</th>
+          <th>10am-11am</th>
+          <th>11am-12noon</th>
+          <th>12noon-1pm</th>
+          <th>1pm-2pm</th>
+          <th>2pm-3pm</th>
+          <th>3pm-4pm</th>
+        </tr>
+      </thead>
+      <tbody>
+        {Object.entries(timeTable).map(([day, slots]) => (
+          <tr key={day}>
+            <td>{day}</td>
+            {[9, 10, 11,12, 13, 14, 15].map((time) => (
+              <td key={time}>
+                {slots[time] ? (
+                  <>
+                    <p>{slots[time].subject}</p>
+                  </>
+                ) : (
+                  "Break"
+                )}
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+  
 
-const rootElement = ReactDOM.createRoot(document.getElementById("root"));
+const rootElement = ReactDOM.createRoot(document.getElementById('root'));
 rootElement.render(<App />);

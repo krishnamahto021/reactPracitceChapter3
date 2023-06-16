a8d531a9deb92baae153b8e00df63611c7bbc6bf
KRISHNA MAHATO <mkrishna98212@gmail.com>
2023-06-16 16:20:31 +0530

    show quote and its author according to the form

--- 1.js
+++ 1.js
@@ -1,61 +1,50 @@
-const traffic = [0,0,0,0]; // index 0 is for car and so on
+const quotes = [];
 
-function handleIncrease(index){
-    traffic[index]++;
-    rootElement.render(<App/>)
-}
+let quoteRef = React.createRef();
+let nameRef = React.createRef();
 
-function handleDecrease(index){
-    if(traffic[index]<=0){
-        window.alert('No Vehicles in parking!');
-    }else{
-        traffic[index]--;
-    }
+
+function handleSubmit(event){
+    event.preventDefault();
+    let newQuote = ({
+        name:nameRef.current.value,
+        quote:quoteRef.current.value
+    });
+    quotes.push(newQuote);
     rootElement.render(<App/>);
-
 }
 
+const clearInputs = () => {
+  nameRef.current.value = "";
+  quoteRef.current.value = "";
+};
 
+const Form = () => (
+    <>
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder='Quote' ref={quoteRef} />
+            <input type="text" placeholder='Name' ref={nameRef} />
+            <button type='submit'>Submit</button>
+        </form>
+    </>
+)
+
+// create handleQuote function here
 
 const App = () => (
-    <>
-      <p>List of all the vehicles count in the parking.</p>
-      <ol>
-        <li>
-          <p>
-            <b>Car:{traffic[0]} </b> &emsp;
-            {/* Add increase and decrease count buttons here */}
-            <button onClick={()=>handleIncrease(0)}>Increase</button>
-            <button onClick={()=>handleDecrease(0)}>Decrease</button>
-          </p>
-        </li>
-        <li>
-          <p>
-            <b>Bike:{traffic[1]} </b> &emsp;
-            {/* Add increase and decrease count buttons here */}
-            <button onClick={()=>handleIncrease(1)}>Increase</button>
-            <button onClick={()=>handleDecrease(1)}>Decrease</button>
-          </p>
-        </li>
-        <li>
-          <p>
-            <b>Scooty:{traffic[2]} </b> &emsp;
-            {/* Add increase and decrease count buttons here */}
-            <button onClick={()=>handleIncrease(2)}>Increase</button>
-            <button onClick={()=>handleDecrease(2)}>Decrease</button>
-          </p>
-        </li>
-        <li>
-          <p>
-            <b>Bicycle:{traffic[3]} </b> &emsp;
-            {/* Add increase and decrease count buttons here */}
-            <button onClick={()=>handleIncrease(3)}>Increase</button>
-            <button onClick={()=>handleDecrease(3)}>Decrease</button>
-          </p>
-        </li>
-      </ol>
-    </>
-  );
+  <div className="App">
+    <h2>Quotify</h2>
+    <Form/>
+    <div className="quotes">
+      {quotes.map((q, i) => (
+        <div key={i}>
+          <i>"{q.quote}"</i>
+          <b>~ {q.name}</b>
+        </div>
+      ))}
+    </div>
+  </div>
+);
 
-  const rootElement = ReactDOM.createRoot(document.getElementById("root"));
-  rootElement.render(<App />);
+const rootElement = ReactDOM.createRoot(document.getElementById("root"));
+rootElement.render(<App />);
